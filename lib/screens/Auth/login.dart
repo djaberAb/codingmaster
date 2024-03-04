d1d4e0340e7bd17d90aced5f9b35a6a9b06df5d9
djaberAb <abbas_djaber@outlook.com>
2024-03-04 12:05:40 +0100

    .

--- lib/screens/Auth/login.dart
+++ lib/screens/Auth/login.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,22 +18,45 @@ class LoginPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const TextField(
+            TextField(
+              controller: usernameController,
               decoration: InputDecoration(
-                labelText: 'Email',
+                labelText: 'username',
               ),
             ),
             SizedBox(height: 16.0),
-            const TextField(
+            TextField(
               obscureText: true,
+              controller: passwordController,
               decoration: InputDecoration(
                 labelText: 'Password',
               ),
             ),
             SizedBox(height: 16.0),
             ElevatedButton(
-              onPressed: () {
-                // TODO: Implement login logic
+              onPressed: () async {
+                String username = usernameController.text;
+                String password = passwordController.text;
+                final response = await http.post(
+                  Uri.parse('https://your-api-url.com/login'),
+                  body: {
+                    'username': username,
+                    'password': password,
+                  },
+                );
+                if (response.statusCode == 200) {
+                  final Map<String, dynamic> data = jsonDecode(response.body);
+                  final String token = data['token'];
+
+                  // Store the token securely in local storage
+                  final prefs = await SharedPreferences.getInstance();
+                  await prefs.setString('auth_token', token);
+
+                  // Navigate to the next screen or perform actions after successful login
+                } else {
+                  // Handle login failure
+                  print('Login failed: ${response.statusCode}');
+                }
               },
               child: Text('Login'),
             ),
